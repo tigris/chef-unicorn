ff6594494e052bdc7d5a23a3fbafe8a3c56159cf
Danial Pearce <git@tigris.id.au>
2012-07-26 13:34:06 +1000

    Remove all the Proc hacks since I know a bit more about how defaults work now.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,10 @@
 node['unicorn']['installs'].each do |install|
-  # Make sure the install defaults come across for each unicorn install
+
+  # Since a lot of defaults rely on app_root, set it and reload defeaults
+  node.set['unicorn']['app_root'] = install['app_root']
+  node.load_attribute_by_short_filename('default', 'unicorn')
+
+  # Apply the defaults for each unicorn install
   install['config'] ||= {}
   %w(rack_env user group pid service command).each do |k|
     install[k] ||= node['unicorn'][k]
@@ -8,49 +13,34 @@ node['unicorn']['installs'].each do |install|
     install['config'][k] ||= node['unicorn']['config'][k]
   end
 
-  # installure defaults, tricky since some defaults are dependant on others and we have interpolation!
-  parsed_pid         = install['pid']    .is_a?(Proc) ? install['pid']    .call(install['app_root']) : install['pid']
-  parsed_service     = install['service'].is_a?(Proc) ? install['service'].call(install['rack_env']) : install['service']
-  parsed_config      = install['config']['path']       .is_a?(Proc) ? install['config']['path']       .call(install['app_root']) : install['config']['path']
-  parsed_stdout_path = install['config']['stdout_path'].is_a?(Proc) ? install['config']['stdout_path'].call(install['app_root']) : install['config']['stdout_path']
-  parsed_stderr_path = install['config']['stderr_path'].is_a?(Proc) ? install['config']['stderr_path'].call(install['app_root']) : install['config']['stderr_path']
-  parsed_command     = install['command'].is_a?(Proc) ? install['command'].call(install['app_root'], install['rack_env'], parsed_config) : install['command']
-
-  # Setup the boot time flags
-  bash "update-rc.d #{parsed_service} defaults" do
-    user 'root'
-    code "update-rc.d #{parsed_service} defaults"
-    action :nothing
-  end
-
   # Create the init.d script
-  template "/etc/init.d/#{parsed_service}" do
+  template "/etc/init.d/#{install['service']}" do
     source 'unicorn.erb'
     variables(
       :root    => install['app_root'],
       :env     => install['rack_env'],
       :user    => install['user'],
-      :pid     => parsed_pid,
-      :command => parsed_command
+      :pid     => install['pid'],
+      :command => install['command']
     )
     mode '775'
-    notifies :run, resources(:bash => "update-rc.d #{parsed_service} defaults")
   end
 
-  # Setup the chef service
-  service parsed_service do
+  # Setup the service to run at boot. We can't start it yet cos no config,
+  # but we need to enable it so the config can notify the restarter.
+  service install['service'] do
     supports [:start, :restart, :reload, :stop, :status]
     action :enable
   end
 
   # Create the install if necessary
-  template parsed_config do
+  template install['config']['path'] do
     only_if   { install['config']['generate'] }
     source    'config.rb.erb'
     user      install['user']
     group     install['group']
     variables(
-      :identifier        => parsed_service,
+      :identifier        => install['service'],
       :listen            => install['config']['listen'],
       :user              => install['user'],
       :group             => install['group'],
@@ -61,15 +51,16 @@ node['unicorn']['installs'].each do |install|
       :before_exec       => install['config']['before_exec'],
       :before_fork       => install['config']['before_fork'],
       :after_fork        => install['config']['after_fork'],
-      :pid               => parsed_pid,
-      :stderr_path       => parsed_stderr_path,
-      :stdout_path       => parsed_stdout_path
+      :pid               => install['pid'],
+      :stderr_path       => install['config']['stderr_path'],
+      :stdout_path       => install['config']['stdout_path']
     )
     mode '755'
-    notifies :restart, resources(:service => parsed_service), :delayed
+    notifies :restart, resources(:service => install['service']), :delayed
   end
 
-  service parsed_service do
+  # Start 'er up.
+  service install['service'] do
     action :start
   end
 end
